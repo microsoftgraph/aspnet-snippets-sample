90866793e41200384ca97f65521416893b2a0125
Christoph Wille <christoph.wille@gmail.com>
2017-08-29 10:42:32 +0200

    Add support for open extensions. Sample workflow modeled around sample https://developer.microsoft.com/en-us/graph/docs/concepts/extensibility_open_users
    Not using resources yet. One (possible) bug in client SDK 1.4 for UpdateAsync not returning Extension.

---
+++ Graph-ASPNET-46-Snippets/Microsoft Graph ASPNET Snippets/Controllers/ExtensionsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Graph;
+using Microsoft_Graph_ASPNET_Snippets.Helpers;
+using Microsoft_Graph_ASPNET_Snippets.Models;
+using Resources;
+
+namespace Microsoft_Graph_ASPNET_Snippets.Controllers
+{
+    [Authorize]
+    public class ExtensionsController : Controller
+    {
+        ExtensionsService extensionsService = new ExtensionsService();
+
+        // Open extensions sample built around https://developer.microsoft.com/en-us/graph/docs/concepts/extensibility_open_users
+        public ActionResult Index()
+        {
+            return View("Extensions");
+        }
+
+        private static readonly string extensionName = "com.contoso.roamingSettings";
+        public async Task<ActionResult> AddOpenExtensionToMe()
+        {
+            ResultsViewModel results = new ResultsViewModel();
+            try
+            {
+                GraphServiceClient graphClient = SDKHelper.GetAuthenticatedClient();
+
+                results.Items = await extensionsService.AddOpenExtensionToMe(graphClient,
+                    extensionName,
+                    new Dictionary<string, object>()
+                            { {"theme", "dark"}, {"color","purple"}, {"lang","Japanese"}});
+            }
+            catch (ServiceException se)
+            {
+                if (se.Error.Message == Resource.Error_AuthChallengeNeeded) return new EmptyResult();
+                return RedirectToAction("Index", "Error", new { message = string.Format(Resource.Error_Message, Request.RawUrl, se.Error.Code, se.Error.Message) });
+            }
+
+            return View("Extensions", results);
+        }
+
+        public async Task<ActionResult> GetOpenExtensionsForMe()
+        {
+            ResultsViewModel results = new ResultsViewModel();
+            try
+            {
+                GraphServiceClient graphClient = SDKHelper.GetAuthenticatedClient();
+
+                results.Items = await extensionsService.GetOpenExtensionsForMe(graphClient);
+            }
+            catch (ServiceException se)
+            {
+                if (se.Error.Message == Resource.Error_AuthChallengeNeeded) return new EmptyResult();
+                return RedirectToAction("Index", "Error", new { message = string.Format(Resource.Error_Message, Request.RawUrl, se.Error.Code, se.Error.Message) });
+            }
+
+            return View("Extensions", results);
+        }
+
+        public async Task<ActionResult> UpdateOpenExtensionForMe()
+        {
+            ResultsViewModel results = new ResultsViewModel();
+            try
+            {
+                GraphServiceClient graphClient = SDKHelper.GetAuthenticatedClient();
+
+                // For updating a single dictionary item, you would first need to retrieve & then update the extension
+                results.Items = await extensionsService.UpdateOpenExtensionForMe(graphClient,
+                    extensionName,
+                    new Dictionary<string, object>()
+                        { {"theme", "light"}, {"color","yellow"}, {"lang","Swahili"}});
+            }
+            catch (ServiceException se)
+            {
+                if (se.Error.Message == Resource.Error_AuthChallengeNeeded) return new EmptyResult();
+                return RedirectToAction("Index", "Error", new { message = string.Format(Resource.Error_Message, Request.RawUrl, se.Error.Code, se.Error.Message) });
+            }
+
+            return View("Extensions", results);
+        }
+
+        public async Task<ActionResult> DeleteOpenExtensionForMe()
+        {
+            ResultsViewModel results = new ResultsViewModel();
+            try
+            {
+                GraphServiceClient graphClient = SDKHelper.GetAuthenticatedClient();
+
+                await extensionsService.DeleteOpenExtensionForMe(graphClient, extensionName);
+
+                results.Items = new List<ResultsItem>() { new ResultsItem() { Display = $"{extensionName} deleted"}};
+            }
+            catch (ServiceException se)
+            {
+                if (se.Error.Message == Resource.Error_AuthChallengeNeeded) return new EmptyResult();
+                return RedirectToAction("Index", "Error", new { message = string.Format(Resource.Error_Message, Request.RawUrl, se.Error.Code, se.Error.Message) });
+            }
+
+            return View("Extensions", results);
+        }
+    }
+}
---
+++ Graph-ASPNET-46-Snippets/Microsoft Graph ASPNET Snippets/Models/ExtensionsService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Graph;
+using Newtonsoft.Json;
+using Resources;
+using WebGrease.Css.Extensions;
+
+namespace Microsoft_Graph_ASPNET_Snippets.Models
+{
+    public class ExtensionsService
+    {
+        public async Task<List<ResultsItem>> AddOpenExtensionToMe(GraphServiceClient graphClient, string extensionName,
+            Dictionary<string, object> data)
+        {
+            var openExtension = new OpenTypeExtension
+            {
+                ExtensionName = extensionName,
+                AdditionalData = data
+            };
+
+            var result = await graphClient.Me.Extensions.Request().AddAsync(openExtension);
+
+            return new List<ResultsItem>() { new ResultsItem()
+                {
+                    Display = result.Id,
+                    Properties = (Dictionary<string,object>)result.AdditionalData
+                }
+            };
+        }
+
+        public async Task<List<ResultsItem>> GetOpenExtensionsForMe(GraphServiceClient graphClient)
+        {
+            var result = await graphClient.Me.Extensions.Request().GetAsync();
+
+            return result.CurrentPage.Select(r => new ResultsItem()
+            {
+                Display = r.Id,
+                Properties = (Dictionary<string, object>)r.AdditionalData
+            }).ToList();
+        }
+
+        public async Task<List<ResultsItem>> UpdateOpenExtensionForMe(GraphServiceClient graphClient, string extensionName,
+            Dictionary<string, object> data)
+        {
+            var openExtension = new OpenTypeExtension
+            {
+                ExtensionName = extensionName,
+                AdditionalData = data
+            };
+
+            // BUG: Returns null instead of Extension
+            var result = await graphClient.Me.Extensions[extensionName].Request().UpdateAsync(openExtension);
+
+            return new List<ResultsItem>() { new ResultsItem()
+                {
+                    Display = result.Id,
+                    Properties = (Dictionary<string,object>)result.AdditionalData
+                }
+            };
+        }
+
+        public async Task DeleteOpenExtensionForMe(GraphServiceClient graphClient, string extensionName)
+        {
+            await graphClient.Me.Extensions[extensionName].Request().DeleteAsync();
+        }
+    }
+}
